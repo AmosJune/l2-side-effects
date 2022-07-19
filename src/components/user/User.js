646062b5b3dcaa3678d8549a94bebd5faa17382a
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-19 15:40:11 +0300

    Done with class today

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,23 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 const RANDOM_USER_API = "https://randomuser.me/api"
 
-function User(props){
+function User(){
 
-    const [name, setName] = useState("sample name")
-    const [email, setEmail] = useState("sample email")
+    const [name, setName] = useState("name")
+    const [email, setEmail] = useState("email")
     const [count, setCount] = useState(0)
 
     function updateCount(currentCount){
         setCount(currentCount + 1)
     }
 
+function randomize(){
+    fetch(RANDOM_USER_API)
+        .then((resp) => resp.json())
+        .then((jsonData) => {
+            const results = jsonData.results
+            const user = results[0]
+            const userName = `${user.name.title}: ${user.name.first} ${user.name.last}`
+            const userEmail = `${user.email}`
+            setName(userName)
+            setEmail(userEmail)
+        })
+}
+
+    useEffect(
+        randomize, [count]
+    )
 
     return(
         <div>
             <center>
-                <h2>NAME: {name}</h2>
-                <h5>EMAIL: {email}</h5>
+                <h2>{name}</h2>
+                <h5>{email}</h5>
                 <button onClick={() => updateCount(count)}>RANDOMIZE</button>
             </center>
         </div>
